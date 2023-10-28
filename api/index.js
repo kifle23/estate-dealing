ed37c1f8a43d3336f82a10aa97ec5fcf3c37480e
kifle23 <kiflekifle23@gmail.com>
2023-10-28 15:35:50 +0300

    Connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-const app= express();
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.log(err);
+});
+
+const app = express();
 
 app.listen(3000, () => {
     console.log('Server on port 3000');
