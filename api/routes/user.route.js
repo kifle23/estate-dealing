77fb14f4111b3459e4541953e5b7c656b9b2277b
kifle23 <kiflekifle23@gmail.com>
2023-11-06 17:16:59 +0300

    Create route for update user

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { test } from '../controllers/user.controller.js';
+import { test, updateUser } from '../controllers/user.controller.js';
+import { verifyUser } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.get('/test', test);
+router.post('/update/:id', verifyUser, updateUser);
 
 export default router;
